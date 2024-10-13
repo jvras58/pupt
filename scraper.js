b864f2a40bace0c48625dd8cf08477dd46a74cd1
Jonathas Vinicius <jonathas.vinicius@ufpe.br>
2024-10-13 00:44:38 +0000

    refactor: remove commented code and clean up scraper.js

--- scraper.js
+++ scraper.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const path = require('path');
 
 (async () => {
-  // Inicia o navegador com o caminho do executável do Chromium, a flag --no-sandbox e desativa o modo headless
   const browser = await puppeteer.launch({
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
     args: ['--no-sandbox'],
@@ -11,28 +10,21 @@ const path = require('path');
   });
   const page = await browser.newPage();
 
-  // Vai até o Google
   await page.goto('https://www.google.com');
 
-  // Aguarda o campo de pesquisa estar presente
-  await page.waitForSelector('#APjFqb'); // Utiliza o id do textarea
+  await page.waitForSelector('#APjFqb');
 
-  // Digita "ping" na barra de pesquisa e aperta Enter
-  await page.type('#APjFqb', 'ping'); // Digita 'ping'
-  await page.keyboard.press('Enter');  // Simula o pressionamento de Enter
+  await page.type('#APjFqb', 'ping');
+  await page.keyboard.press('Enter');
 
-  // Aguarda o carregamento dos resultados da pesquisa
   await page.waitForSelector('#search');
 
-  // Cria a pasta 'screenshots' se não existir
   const screenshotsDir = path.resolve(__dirname, 'screenshots');
   if (!fs.existsSync(screenshotsDir)) {
     fs.mkdirSync(screenshotsDir);
   }
 
-  // Tira uma captura de tela dos resultados da pesquisa e salva no workspace
   await page.screenshot({ path: path.join(screenshotsDir, 'google_search.png') });
 
-  // Fecha o navegador
   await browser.close();
 })();
